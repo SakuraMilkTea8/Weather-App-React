ee2b1053027ac4730c4c4f4debe8dee6bf3f0b0a
SakuraMilkTea8 <73587894+SakuraMilkTea8@users.noreply.github.com>
2022-02-24 19:04:46 +0900

    Fixed superficial unit conversion + added working technical unit conversion

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -19,24 +19,25 @@ export default function WeatherTemperature(props) {
         <div className="todays-weather number">{Math.round(props.celsius)}</div>
         <div className="todays-weather letters">
           <span className="unit">
-            <a href="/" onClick={showFahrenheit()}>
-              ℃
-            </a>{" "}
-            | ℉
+            ℃ |{" "}
+            <a href="/" onClick={showFahrenheit}>
+              ℉
+            </a>
           </span>
         </div>
       </span>
     );
   } else {
+    let fahrenheit = (props.celsius * 9) / 5 + 32;
     return (
       <span className="todays-weather">
-        <div className="todays-weather number">{Math.round(props.celsius)}</div>
+        <div className="todays-weather number">{Math.round(fahrenheit)}</div>
         <div className="todays-weather letters">
           <span className="unit">
-            ℃ |{" "}
-            <a href="/" onClick={showCelsius()}>
-              ℉
-            </a>
+            <a href="/" onClick={showCelsius}>
+              ℃
+            </a>{" "}
+            | ℉
           </span>
         </div>
       </span>
